24bb0543939e6a2d33312224116ce2caa39a3720
André Rodrigues <andre.urbanico@gmail.com>
2020-08-05 23:13:56 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,69 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Criando function component 
+class App extends React.Component {
+
+  // Criando construtor
+  constructor(props) {
+
+    // Executando o construtor da Super Class
+    super(props)
+
+    // Definindo o estado inicial
+    this.state = {
+      clicked: false
+    }
+
+  }
+
+  // Função para alterar o estado
+  changeClicked() {
+    this.setState({ clicked: !this.state.clicked })
+  }
+
+  // Função para recurepar valor dependendo do estado
+  getLabel() {
+    if (this.state.clicked) {
+      return "ESTÁ LIGADO CLASS"
+    } else {
+      return "ESTÁ DESLIGADO CLASS"
+    }
+  }
+
+  // Função que renderiza o componente
+  render() {
+
+    // // Criando elemento 
+    // let elem = React.createElement(
+    //   'button', // Tipo
+    //   { onClick: () => this.changeClicked() }, // Atributos
+    //   this.getLabel() // Conteúdo
+    // )
+
+    // // Retornando elemento
+    // return elem
+
+    return (
+      <div className="row">
+        
+        {(this.state.clicked) ? (
+          <div className="col-sm-6">
+            <button onClick={() => this.changeClicked()} className="btn btn-primary">
+              {this.getLabel()}
+            </button>
+          </div>
+        ) : (
+          <div className="col-sm-6">
+            <button onClick={() => this.changeClicked()} className="btn btn-danger">
+              {this.getLabel()}
+            </button>
+          </div>
+        )}
+      </div>
+    )
+  }
+
 }
 
 export default App;
