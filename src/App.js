74a8fefb3d8853ed5d75b4f4eb4d5e7ee7b13bc7
André Rodrigues <andre.urbanico@gmail.com>
2020-08-12 22:37:39 -0300

    finished aula 02

--- src/App.js
+++ src/App.js
@@ -1,44 +1,74 @@
 import React from 'react';
 import './App.css';
+import Header from './components/header/header.component'
 
 // Criando c component 
 class App extends React.Component {
 
-  // Criando construtor
-  constructor(props) {
+    // Criando construtor
+    constructor(props) {
 
-    // Executando o construtor da Super Class
-    super(props)
+        // Executando o construtor da Super Class
+        super(props)
 
-    // Definindo o estado inicial
-    this.state = {
-      clicked: false
+        // Definindo o estado inicial
+        this.state = {
+            clicked: false,
+            buttonClass: "botao-desligado"
+        }
     }
 
-  }
+    // Função para alterar o estado
+    changeClicked() {
 
-  // Função para alterar o estado
-  changeClicked() {
-    this.setState({ clicked: !this.state.clicked })
-  }
+        let clicked = !this.state.clicked
+        let buttonClass = {}
 
-  // Função para recurepar valor dependendo do estado
-  getLabel() {
-    if (this.state.clicked) {
-      return "ESTÁ LIGADO"
-    } else {
-      return "ESTÁ DESLIGADO"
+        if (clicked) {
+            buttonClass = "botao-ligado"
+        } else {
+            buttonClass = "botao-desligado"
+        }
+
+        this.setState({ clicked: clicked, buttonClass: buttonClass })
     }
-  }
 
-  // Função que renderiza o componente
-  render() {
-    return (
-      <button onClick={() => this.changeClicked()} className="btn btn-primary">
-        {this.getLabel()}
-      </button>
-    )
-  }
+    // Função para recurepar valor dependendo do estado
+    getLabel() {
+        if (this.state.clicked) {
+            return "ESTÁ LIGADO"
+        } else {
+            return "ESTÁ DESLIGADO"
+        }
+    }
+
+
+    // Função que renderiza o componente
+    render() {
+        return (
+            <section>
+                <Header></Header>
+                <div className="row">
+                    <div className="col-sm-12 d-flex justify-content-center">
+                        <button onClick={() => this.changeClicked()} className={this.state.buttonClass}>
+                            {this.getLabel()}
+                        </button>
+                    </div>
+                    <div className="col-sm-12 d-flex justify-content-center mt-5">
+                        {(this.state.clicked) ? (
+                            <div className="alert alert-primary" role="alert">
+                                O Botão está ligado
+                            </div>
+                        ) : (
+                            <div className="alert alert-danger" role="alert">
+                                O Botão está desligado
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </section>
+        )
+    }
 
 }
 
